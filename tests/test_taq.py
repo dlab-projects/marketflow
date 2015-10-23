694ef996ae8315559cbc48880bc8e6c98ba7cb17
Dav Clark <davclark@berkeley.edu>
2015-10-23 09:55:20 -0700

    Added a test for data

--- tests/test_taq.py
+++ tests/test_taq.py
@@ -1,4 +1,15 @@
+from os import listdir
 import taq
 
-def test_nothing():
-    assert True
+
+def test_data_available():
+    '''Test that our sample data is present
+
+    Currently, data should be exactly the data also available on Box in the
+    taq-data folder maintained by D-Lab. These data are copyrighted, so if
+    you're not a member of the D-Lab, you'll likely need to arrange your own
+    access!
+    '''
+    test_data_contents = listdir('test-data')
+    assert 'EQY_US_ALL_BBO_20111101.zip' in test_data_contents
+    assert 'EQY_US_ALL_BBO_20140206.zip' in test_data_contents
