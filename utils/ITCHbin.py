90efc8b539b1c9278f0f418b1d012077d1c9a716
Dav Clark <davclark@berkeley.edu>
2016-01-27 16:05:58 -0800

    Refactored ITCHbin

--- utils/ITCHbin.py
+++ utils/ITCHbin.py
@@ -1,44 +1,99 @@
 #!/usr/bin/env python3
 
-import sys
+'''This script is based on the ITCH v5.0 spec.
+
+It assumes their squirrelly binary message format.'''
+
 import struct
 import gzip
 
-# Currently, we redundantly unpack and keep the record type indicator
 
-# Struct documented here: https://docs.python.org/3.4/library/struct.html
+class ITCHv5:
+    '''Convert an ITCH v5.0 file to reasonable records
 
-# ITCH record formats detailed in section 4.1 here:
-# http://www.nasdaqtrader.com/content/technicalsupport/specifications/dataproducts/NQTVITCHspecification.pdf
+    Currently, we redundantly unpack and keep/print the record type indicator
 
-# I'm using the equivalent p/s codes to indicate unevaluated bytes vs actual
-# ascii / "alpha" data
+    Struct documented here:
+    https://docs.python.org/3.4/library/struct.html#format-characters
 
-# Using ord here to make lookups more straightforward with bytes (not sure why
-# rec[0] below doesn't return a byte...)
-std_prefix = '>c2h6p'
-rec_types = {ord('S'): std_prefix + 'c',              # System Event Message
-             ord('R'): std_prefix + '8s2ci2c2s5cic',  # Stock Directory Message
-             }
+    ITCH v5.0 record formats detailed in section 4.1 here:
+    http://www.nasdaqtrader.com/content/technicalsupport/specifications/dataproducts/NQTVITCHspecification.pdf
 
-with gzip.GzipFile(sys.argv[1]) as infile:
-    while(True):
-        rec_len, = struct.unpack('>h', infile.read(2))
-        # print('rec_len:', rec_len)
-        rec = infile.read(rec_len)
+    I'm using the equivalent p/s codes to indicate unevaluated bytes vs actual
+    ascii / "alpha" data
+
+    Using ord here to make lookups more straightforward with bytes (not sure
+    why rec[0] below doesn't return a byte...)
+    '''
+
+    std_prefix = '>c2h6p'
+    rec_types = {ord('S'): std_prefix + 'c',              # System Event Message
+                 ord('R'): std_prefix + '8s2ci2c2s5cic',  # Stock Directory Message
+                 }
+
+    def __init__(self, fname):
+        self.fname = fname
+
+    def records(self):
+        '''Generator that returns unpacked records from self.fname
+
+        Returns records as a list. Note that ascii strings are returned as
+        bytes.
+        '''
+        with gzip.GzipFile(self.fname) as infile:
+            while(True):
+                # XXX A different idea (that would make it easier to use numpy)
+                # would be to read 3 bytes, and also get the record type in
+                # this initial read.
+                rec_len, = struct.unpack('>h', infile.read(2))
+                # .read() just returns '' when it's at EOF
+                if rec_len == '':
+                    break
+                # print('rec_len:', rec_len)
+                rec = infile.read(rec_len)
+                try:
+                    # print('rec_type:', rec[0])
+                    fmt = self.rec_types[rec[0]]
+                    # print(fmt)
+                    unpacked_rec = list( struct.unpack(fmt, rec) )
+
+                    # This unpacks that annoying 6-byte int timestamp that
+                    # struct can't handle (maybe we should be using numpy?)
+                    unpacked_rec[3] = int.from_bytes(unpacked_rec[3], 'big')
+
+                    yield unpacked_rec
+
+                except KeyError:
+                    # Silently ignore unknown record types
+                    # (at least until we have them all)
+                    # print('Unkown!')
+                    pass
+
+    def to_string(self, b):
+        '''Try to decode b to ascii
+
+        This is why people don't like Python 3
+        '''
         try:
-            # print('rec_type:', rec[0])
-            fmt = rec_types[rec[0]]
-            # print(fmt)
-            unpacked_rec = struct.unpack(fmt, rec)
+            return b.decode('ascii')
+        except AttributeError:
+            return str(b)
 
-            # This unpacks that annoying 6-byte int timestamp that struct can't
-            # handle (maybe we should be using numpy?)
-            unpacked_rec = list(unpacked_rec)
-            unpacked_rec[4] = int.from_bytes(unpacked_rec[4], 'big')
-            print(unpacked_rec)
-        except KeyError:
-            # Silently ignore unknown record types
-            # (at least until we have them all)
-            # print('Unkown!')
-            pass
+    def print_records(self):
+        '''This could be redefined to get what kind of output you like'''
+        # XXX This is rather insane - converting from bytes to text
+        # so that SAS can parse the text back into bytes
+        for rec in self.records():
+            # Note - we have to do this after unpacking the above
+            # 6-byte integer
+            print(','.join(self.to_string(r) for r in rec))
+
+if __name__ == '__main__':
+    from sys import argv, exit
+    try:
+        itch = ITCHv5(argv[1])
+    except IndexError:
+        print('Usage: ITCHbin.py <ITCH v5.0 file>')
+        exit(1)
+
+    itch.print_records()
