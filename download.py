a602ac61bd2cb00376cbdf68c2760b4c3a84fc12
rayjyang <Raymond@airbears2-10-142-127-213.airbears2.1918.berkeley.edu>
2016-02-19 17:54:14 -0800

    automate downloading of public files

--- download.py
+++ download.py
@@ -2,6 +2,16 @@ from boxsdk import Client, OAuth2
 from boxsdk.network.default_network import DefaultNetwork
 from pprint import pformat
 
+import requests
+
+print('Downloading public test data...')
+PUBLIC_URL = 'https://berkeley.box.com/shared/static/c4tqpu0h601b2ypimpst3uutfc8e4s93.zip'
+
+# Writes test_data_public.zip to test-data/
+resp = requests.get(PUBLIC_URL)
+with open('test-data/test_data_public.zip', 'wb') as test_data:
+    test_data.write(resp.content)
+
 CLIENT_ID = None
 CLIENT_SECRET = None
 DEVELOPER_TOKEN = None
@@ -10,6 +20,12 @@ with open('boxauth.cfg', 'r') as boxauth:
     CLIENT_ID = boxauth.readline()
     CLIENT_SECRET = boxauth.readline()
     DEVELOPER_TOKEN = boxauth.readline()
+
+# # Forward user to Box's login page
+# req = 'https://app.box.com/api/oauth2/authorize?response_type=code&client_id='+CLIENT_ID+'&state=security_token%3DKnhMJatFipTAnM0nHlZA'
+# resp = requests.get(req)
+# print(resp.content)
+
 
 class LoggingNetwork(DefaultNetwork):
     def request(self, method, url, access_token, **kwargs):
@@ -33,4 +49,9 @@ client = Client(oauth, LoggingNetwork())
 
 root_folder = client.folder(folder_id='0').get()
 items = client.folder(folder_id='0').get_items(limit=100, offset=0)
+
+shared_link = client.folder(folder_id='3800889110').get_shared_link()
+
+
+
 
