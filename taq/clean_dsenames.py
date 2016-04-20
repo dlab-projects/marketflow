b9f0e2d8d09c18f1e3d0debf912454ffaae8ee91
Max Eddy <maxeddy@airbears2-10-142-39-45.airbears2.1918.berkeley.edu>
2016-04-20 15:55:29 -0700

    get_permno method established

--- taq/clean_dsenames.py
+++ taq/clean_dsenames.py
@@ -10,12 +10,20 @@ class Permno_Map(object):
         # For now, it's easier to debug with smaller functions run one at a
         # time.
         # self.process(dsenames)
+        self.nasdaq = dsenames.PRIMEXCH == "Q"
 
-    def get_permno(self, root, date):
+    def get_permno(self, cd, root, date):
         '''Get the permno for a given symbol root.
 
         Remember, permno does not change with suffix.'''
-        raise NotImplementedError
+        that_permno = cd[cd.SYM_ROOT == root]
+        permno_on_date = that_permno.loc[(cd.BEGDATE <= date) 
+                                         & (cd.ENDDATE >= date)
+                                         , 'PERMNO'] 
+        if len(permno_on_date > 0):
+            return permno_on_date
+        else:
+            raise NotImplementedError
 
     def process(self, dsenames):
         '''Run all processing steps in a reasonable order'''
@@ -32,14 +40,21 @@ class Permno_Map(object):
         date : int
             Not really an int, but the naïve conversion from the datestring.
         regular : bool
-            Limit to "regular" stocks?
+            Limit to "regular" stocks. i.e. the security is past the "When-Issued" 
+            stage and the company is not going through bankruptcy proceedings)
         active : bool
-            Limit to "active" stocks?
+            Limit to entries for stocks that are actively trading
         beneficial : bool
-            XXX
+            If =False, we exclude stocks that are "shares of beneficial interest", 
+            which indicates that the stocks are not trading normally due to their
+            inclusion in some sort of trust. 
         when_issued : bool
-            XXX
+            If =False, we exclude when_issued shares, which have been approved for 
+            trading but have not yet begun trading actively
         '''
+        bad_permnos = [14209, 15141, 91845]
+        # Here, we index out any securities whose records don't make sense
+        dsenames = dsenames[~dsenames['PERMNO'].isin(bad_permnos)]
         # By default, we only include securities that were trading at some
         # point from January 2010 onwards
         dsenames = dsenames[dsenames['NAMEENDT'] >= date]
@@ -53,79 +68,67 @@ class Permno_Map(object):
             dsenames = dsenames[dsenames['TRDSTAT'] == "A"]
 
         dsenames['SYMBOL_LEN'] = dsenames['TSYMBOL'].str.len()
-        dsenames['TICKER_LEN'] = dsenames['TICKER'].str.len()
-        # There are 239 cases where the length of tsymbol does not match up
-        # with the length of ticker For most of these cases, it is because
-        # tsymbol includes a suffix of some sort, while ticker does not
-
         dsenames['LAST'] = dsenames['TSYMBOL'].str[-1]
-        # This grabs the last two elements of tsymbol
         dsenames['LAST2'] = dsenames['TSYMBOL'].str[-2:]
-
-        bad_permnos = [14209, 15141, 91845]
-        # Here, we index out any securities whose records don't make sense
-        dsenames = dsenames[~dsenames['PERMNO'].isin(bad_permnos)]
-
+        
         # The 'S' suffix indicates the "Shares of Beneficial Interest, which do
         # not trade like regular securities The 'V' and 'WI' suffixes indicate
         # "When-Issued" shares, which have been authorized to trade, but have
         # not actually begun trading
         # XXX Maybe make this attribute defined at a "higher level"?
-        self.nasdaq = dsenames.PRIMEXCH == "Q"
-        beneficial = (dsenames['LAST'] == "S") & (dsenames['SYMBOL_LEN'] == 5)
-        whenissued_nasdaq = ((dsenames['LAST'] == "V") &
+        if beneficial == False:
+            ben = (dsenames['LAST'] == "S") & (dsenames['SYMBOL_LEN'] == 5)
+            dsename = dsenames[~ben]
+        if when_issued == False:
+            whenissued_nasdaq = ((dsenames['LAST'] == "V") &
                              (dsenames['SYMBOL_LEN'] == 5) & self.nasdaq)
-        whenissued_nonnasdaq = ((dsenames['LAST2'] == "WI") &
+            whenissued_nonnasdaq = ((dsenames['LAST2'] == "WI") &
                                 (dsenames['SYMBOL_LEN'] > 3) & ~self.nasdaq)
-
-        # Here, we take out the BoWI shares from bot NASDAQ and non-NASDAQ
-        # securities
-        dsenames = dsenames[~(beneficial & whenissued_nasdaq & whenissued_nonnasdaq)]
-
+            dsenames = dsenames[~(whenissued_nasdaq & whenissued_nonnasdaq)]
         return dsenames
 
     def dse_rootsplit(self, dsenames):
-        '''XXX More docstrings!'''
+        '''Splits the root and the suffix into two separate variables,
+        SYM_ROOT and SYM_SUFFIX and flags suffix extraction cases
+        FLAG index:
+            =0 : base case, symbol has no suffix
+            =1 : NASDAQ, share class
+            =2 : NASDAQ, foreign shares or voting/non-voting shares
+            =3 : NASDAQ, reverse stock split
+            =4 : non-NASDAQ, share class suffix
+        Includes manual adjustments for idiosyncratic securities, should
+        be re-evaluated from time to time
+        '''
         # Flag = 0 is our base case (i.e. the ticker symbol has no suffix)
         dsenames['FLAG'] = 0
         # When the ticker has no suffix, the root is just the ticker symbol, and the
         # suffix is an empty string
         dsenames['SYM_ROOT'] = dsenames['TSYMBOL']
         dsenames['SYM_SUFFIX'] = ""
+        dsenames['TICKER_LEN'] = dsenames['TICKER'].str.len()
 
-        # Now begins the subsetting based on cases of symbol suffixes
-
-        # Since we already have our nasdaq variable from earlier, we can create a few
-        # more booleans to help us conduct the subsetting in a vectorized way
-
-        # This first boolean vector will be True for a ticker symbol if its last
-        # character is the same as its share class
         class_equal_last = dsenames.SHRCLS == dsenames.LAST
 
-        # This boolean will be True for all NASDAQ securities with a ticker symbol
-        # longer than 4 characters. 4 is the maximum number of characters for a ticker
-        # symbol on the NASDAQ.
+        # nasdaq_long true for NASDAQ securities with a ticker symbol
+        # longer than 4 characters. 4 is the maximum number of characters for
+        # a ticker symbol on the NASDAQ.
         nasdaq_long = self.nasdaq & (dsenames.SYMBOL_LEN > 4)
 
-        # The first flag will denote securities with ticker symbols that have share
-        # class suffixes, e.g. a company on the NASDAQ that has Class A and Class B
-        # shares
+        # flag1 denotes securities with share class suffixes,
+        # e.g. a company on the NASDAQ that has Class A and Class B shares
 
         flag1 = nasdaq_long & class_equal_last
 
-        # The second flag will denote two different special cases:
+        # flag2 denotes two cases:
         # - Suffixes Y and F denote shares in foreign companies
         # - Suffixes J and K denote voting and non-voting shares, respectively
         flag2 = ~flag1 & nasdaq_long & dsenames.LAST.isin(["Y", "J", "F", "K"])
 
-        # The third flag will denote (NASDAQ) stocks that are currently going through a
-        # reverse stock split. These securities usually keep this ticker symbol for
-        # about three weeks after the reverse stock split takes place.
+        # flag3 denotes stocks going through reverse stock split
+        # these securities keep this ticker symbol for ~3 weeks post-split
         flag3 = ~flag1 & nasdaq_long & (dsenames.LAST == "D")
 
-        # The fourth flag will denote non-NASDAQ stocks that have share class
-        # suffixes.  For non-NASDAQ exchanges (e.g. the New York Stock
-        # Exchange), ticker symbols without suffixes are 3 characters or less.
+        # flag4 denotes non-NASDAQ stocks w/ share class suffix
         flag4 = ~self.nasdaq & (dsenames.SYMBOL_LEN > 3) & class_equal_last
 
         # There is a fifth set of suffixed ticker symbols that do not fit into
@@ -139,8 +142,6 @@ class Permno_Map(object):
                  (dsenames.SYMBOL_LEN - dsenames.TICKER_LEN == 1) &
                  dsenames.LAST.isin(["A", "B", "C", "S"])
                  )
-
-        # Here, we assign the flags to each special case, as described above
 
         dsenames.loc[flag4, "FLAG"] = 4
         dsenames.loc[flag3, "FLAG"] = 3
@@ -179,7 +180,9 @@ class Permno_Map(object):
         return dsenames
 
     def drop_dups(self, dsenames):
-        '''XXX More docstrings!'''
+        '''Consolidates multiple records for same ticker symbol into one
+        by collapsing trading date range
+        '''
         # Finally, we want to ensure that, when the same information is
         # recorded, the date range listed for the record reflects the entire
         # range over which the security was actively trading.
